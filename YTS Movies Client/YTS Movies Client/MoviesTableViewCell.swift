359e008a92d8957fefd2ee2337faa423288c4897
MohamedSh <mohamed@MohamedShs-Mac.local>
2017-12-15 23:33:16 +0200

    creating cocoa touch class MoviesTableViewCell for custom cell

---
+++ YTS Movies Client/YTS Movies Client/MoviesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MoviesTableViewCell.swift
+//  YTS Movies Client
+//
+//  Created by MohamedSh on 12/15/17.
+//  Copyright © 2017 MohamedSh. All rights reserved.
+//
+
+import UIKit
+
+class MoviesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
