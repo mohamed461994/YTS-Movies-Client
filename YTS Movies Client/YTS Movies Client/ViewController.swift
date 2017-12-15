464fa7b82d03ff90a4ea969925bb86cb28beb3d7
MohamedSh <mohamed@MohamedShs-Mac.local>
2017-12-16 00:18:30 +0200

    setting search text field delegate to use it in searching

--- YTS Movies Client/YTS Movies Client/ViewController.swift
+++ YTS Movies Client/YTS Movies Client/ViewController.swift
@@ -8,17 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController , UITableViewDelegate,UITableViewDataSource{
+class ViewController: UIViewController , UITableViewDelegate,UITableViewDataSource,UITextFieldDelegate{
 
+    @IBOutlet weak var searchLTxtField: UITextField!{
+        didSet{
+            searchLTxtField.delegate=self
+        }
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // search for movies
+        return true
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
     }
