96212ac83684de574061b95faa70af35382a45ba
MohamedSh <mohamed@MohamedShs-Mac.local>
2017-12-16 10:35:21 +0200

    creating ViewModel class

---
+++ YTS Movies Client/YTS Movies Client/ViewMode.swift
@@ -0,0 +1,12 @@
+//
+//  ViewMode.swift
+//  YTS Movies Client
+//
+//  Created by MohamedSh on 12/16/17.
+//  Copyright © 2017 MohamedSh. All rights reserved.
+//
+
+import Foundation
+class ViewModel{
+    
+}
