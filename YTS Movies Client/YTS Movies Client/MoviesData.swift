c8f30c1191a5540268e38fe7c25f379e352e37cc
MohamedSh <mohamed@MohamedShs-Mac.local>
2017-12-16 23:06:57 +0200

    creating MoviesData class as Model for MVVM

---
+++ YTS Movies Client/YTS Movies Client/MoviesData.swift
@@ -0,0 +1,12 @@
+//
+//  MoviesData.swift
+//  YTS Movies Client
+//
+//  Created by MohamedSh on 12/16/17.
+//  Copyright © 2017 MohamedSh. All rights reserved.
+//
+
+import Foundation
+class MoviesData{
+    
+}
