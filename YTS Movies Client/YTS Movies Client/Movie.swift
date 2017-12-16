fbeb4025b32cbbdaa4a98c9e8865956e0add9290
MohamedSh <mohamed@MohamedShs-Mac.local>
2017-12-16 23:56:05 +0200

    creating Movie struct to represent each movie data

---
+++ YTS Movies Client/YTS Movies Client/Movie.swift
@@ -0,0 +1,16 @@
+//
+//  Movie.swift
+//  YTS Movies Client
+//
+//  Created by MohamedSh on 12/16/17.
+//  Copyright © 2017 MohamedSh. All rights reserved.
+//
+
+import Foundation
+struct Movie {
+    var id:Int?
+    var imgURL:String?
+    var name:String?
+    var rate:Float?
+    var typeList:[String]=[]
+}
