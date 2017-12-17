39c4b36bafc955032cf66a517862ca3d989fff55
MohamedSh <mohamed@MohamedShs-Mac.local>
2017-12-17 16:30:23 +0200

    create second view controller cocoatouch class MovieDetailesTableViewController

---
+++ YTS Movies Client/YTS Movies Client/MovieDetailesTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MovieDetailesTableViewController.swift
+//  YTS Movies Client
+//
+//  Created by MohamedSh on 12/17/17.
+//  Copyright © 2017 MohamedSh. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailesTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
