a2703ccf4d24261a265925c40715b6373c60775c
MohamedSh <mohamed@MohamedShs-Mac.local>
2017-12-17 18:31:22 +0200

    creating outlets for views in Detailes controller for movie info

--- YTS Movies Client/YTS Movies Client/MovieDetailesTableViewController.swift
+++ YTS Movies Client/YTS Movies Client/MovieDetailesTableViewController.swift
@@ -10,16 +10,30 @@ import UIKit
 
 class MovieDetailesTableViewController: UITableViewController {
     @IBOutlet weak var lblDescription: UILabel!
+    var id:Int?{
+        didSet{updateUI()}
+    }
+    @IBOutlet weak var lblMovieName: UILabel!
+    @IBOutlet weak var lblMovieCategories: UILabel!
+    @IBOutlet weak var lblMovieYear: UILabel!
+    @IBOutlet weak var lblRatting: UILabel!
+    @IBOutlet weak var lblNumberOfLikes: UILabel!
+    @IBOutlet weak var lblNumberOfDownloads: UILabel!
+    @IBOutlet weak var lblMovieDescription: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         if indexPath.row == 1 {
-            print(lblDescription.frame.height)
+            //print(lblDescription.frame.height)
             return CGFloat(lblDescription.frame.height + 150 )
         }else{
             return super.tableView(tableView, heightForRowAt: indexPath)
         }
+    }
+    func updateUI(){
+        
     }
 }
