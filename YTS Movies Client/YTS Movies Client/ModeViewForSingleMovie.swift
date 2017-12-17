169f12e3bbc54f2f77f981587608d4bf48292dfa
MohamedSh <mohamed@MohamedShs-Mac.local>
2017-12-17 22:45:56 +0200

    create ViewModel functions For getting info of the selected movie for second view controller

--- YTS Movies Client/YTS Movies Client/ModeViewForSingleMovie.swift
+++ YTS Movies Client/YTS Movies Client/ModeViewForSingleMovie.swift
@@ -8,10 +8,35 @@
 
 import Foundation
 class ViewModelForSingleMovie{
-    var moviesData:MoviesData?
-    init(){
+    var movieData:MoviesData?
+    init(id:Int){
         let urlForList="https://yts.am/api/v2/movie_details.json"
-        moviesData = MoviesData(url: urlForList,parameters:["movie_id":10])
+        movieData = MoviesData(url: urlForList,parameters:["movie_id":id])
     }
-    
+    func getTitle()->String{
+        return (movieData?.singleMovieDetails?.title)!
+    }
+    func getDescription()->String{
+        return (movieData?.singleMovieDetails?.description)!
+    }
+    func getDownloadsCount()->String{
+        return String((movieData?.singleMovieDetails?.downloads)!)
+    }
+    func getYear()->Int{
+        return (movieData?.singleMovieDetails?.year)!
+    }
+    func getCategores()->String{
+        let lis = (movieData?.singleMovieDetails?.category)!
+        return lis.joined(separator: " / ")
+    }
+    func getRating()->String{
+        let rat = (movieData?.singleMovieDetails?.rating)!
+        return String(rat)
+    }
+    func getImgURL()->String{
+        return (movieData?.singleMovieDetails?.imgURL)!
+    }
+    func getLikesCount()->String{
+        return String((movieData?.singleMovieDetails?.likes)!)
+    }
 }
