3850ffaa34116bda448d0452f5b290df91bbaacf
MohamedSh <mohamed@MohamedShs-Mac.local>
2017-12-18 17:59:43 +0200

    documentation for MoviesData class

--- YTS Movies Client/YTS Movies Client/MoviesData.swift
+++ YTS Movies Client/YTS Movies Client/MoviesData.swift
@@ -4,26 +4,34 @@
 //
 //  Created by MohamedSh on 12/16/17.
 //  Copyright © 2017 MohamedSh. All rights reserved.
-//
 
 import Foundation
 import Alamofire
+/**
+ This class is used to deal with API to get information from yts API
+ */
 class MoviesData{
-    var requestedFromFirstViewController=true
+    /// this varible is to detect if request is from first view controller or not
+    private var requestedFromFirstViewController=true
+    /// this list will contains movies information for the first view controller
     var moviesList:[Movie]=[]
     private var url:String?
-    var requestPageNumberOfMoviesData = 1
+    /// this varible used for pagginatin to get more data from the API
+    private var requestPageNumberOfMoviesData = 1
+    /// this varaible store parameter as dictionary of type [String:Any]
     private var parameters: Parameters?
+    /// this tuble will be used to store mapped data in case of it's request for the second view controller
     var singleMovieDetails:(id:Int,title:String,rating:Float, category:[String],imgURL:String,year:Int,likes:Int,description:String ,downloads:Int
-        )?
-    
+    )?
+    /** this varible use property opservers to push notification when it set */
     private var parseIsDone:Bool?{
         didSet{
-            //push notification to reload data in first view controllet
+            /// push notification to reload data in first view controllet
             let notifiReload = Notification.Name(notificationForReloadTable)
             NotificationCenter.default.post(name: notifiReload, object: nil)
         }
     }
+    /// this varaible is comtained JSON data that came from API
     private var jsonData:Any?{ didSet{parseJSON()} }
     init(url:String , parameters: Parameters) {
         self.url = url
@@ -31,11 +39,11 @@ class MoviesData{
         self.parameters=parameters
         getJSON()
     }
-    func getJSON(){
+    /// this function is used to make the request using Alamofire and assing json return to variable  jsonData this function is using threads t
+    private func getJSON(){
         let utilityQueue=DispatchQueue.global(qos: .utility)
         Alamofire.request(url!, method: .get,parameters: parameters).responseJSON(queue: utilityQueue){ response in
             if let json = response.result.value {
-                //print(json)
                 self.jsonData=json
             }
         }
@@ -43,10 +51,10 @@ class MoviesData{
 }
 
 extension MoviesData{
-    func parseJSON(){
+     /// this function is used to mapping json information to moviesList in case of requesc from first view controller or to tuble called singleMovieDetails if it's the second view controller
+    private func parseJSON(){
         let allData = jsonData as? [String : Any]
         let movesData = allData!["data"] as? [String : Any]
-        //print(movesData!)
         if requestedFromFirstViewController{
             if let movies = movesData!["movies"] as? [[String:Any]]{
                 for movie in movies {
@@ -65,6 +73,10 @@ extension MoviesData{
     }
 }
 extension MoviesData{
+    /**
+     this function used to pagination when used scroll down to last cell in table view
+     this funtion will make new request to the API with parameter page +1 to last page
+     */
     func getMoreDataWhenUserScroll(){
         requestPageNumberOfMoviesData += 1
         parameters!["page"] = requestPageNumberOfMoviesData
@@ -72,7 +84,8 @@ extension MoviesData{
     }
 }
 extension MoviesData{
-    func parsingDataForSeconViewController(movie: [String:Any]){
+    /// this function gets information of a single movie and set it to the tuble singleMovieDetails
+    private func parsingDataForSeconViewController(movie: [String:Any]){
         singleMovieDetails = (id: (movie["id"] as? Int)!
             ,title:(movie["title_english"] as? String)!
             ,rating:(movie["rating"] as? Float)!
@@ -85,10 +98,9 @@ extension MoviesData{
         )
         fireNotificationForSecondViewController()
     }
-    func fireNotificationForSecondViewController(){
-        // post notification when data is ready to second view controller
+    ///  this function used to push notification when data processing is finished
+    private func fireNotificationForSecondViewController(){
         let notifiReload = Notification.Name(notificationForLoadData)
         NotificationCenter.default.post(name: notifiReload, object: nil)
     }
-    
 }
