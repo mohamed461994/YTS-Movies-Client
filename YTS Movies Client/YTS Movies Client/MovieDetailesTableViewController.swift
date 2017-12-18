83b209357d5091a717f90ccc6bceff6b4d9b3ecc
MohamedSh <mohamed@MohamedShs-Mac.local>
2017-12-18 18:39:22 +0200

    documentation for MovieDetailsTableViewController

--- YTS Movies Client/YTS Movies Client/MovieDetailesTableViewController.swift
+++ YTS Movies Client/YTS Movies Client/MovieDetailesTableViewController.swift
@@ -11,13 +11,14 @@ import UIKit
 import Kingfisher
 class MovieDetailesTableViewController: UITableViewController {
     @IBOutlet weak var lblDescription: UITextView!
+    /// this varible is used to get all data for spesific ID
     var id:Int?{
         didSet{
          viewModel = ViewModelForSingleMovie(id: id!)
         }
     }
-    @IBOutlet weak var movieImage: UIImageView!
     var viewModel : ViewModelForSingleMovie?
+    @IBOutlet weak var movieImage: UIImageView!
     @IBOutlet weak var lblMovieName: UILabel!
     @IBOutlet weak var lblMovieCategories: UILabel!
     @IBOutlet weak var lblMovieYear: UILabel!
@@ -25,6 +26,8 @@ class MovieDetailesTableViewController: UITableViewController {
     @IBOutlet weak var lblNumberOfLikes: UILabel!
     @IBOutlet weak var lblNumberOfDownloads: UILabel!
     @IBOutlet weak var lblMovieDescription: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         createObserverForReloadData()
@@ -37,10 +40,16 @@ class MovieDetailesTableViewController: UITableViewController {
             return super.tableView(tableView, heightForRowAt: indexPath)
         }
     }
+    /**
+     This function to create observer to get notifies when data is ready
+     */
     func createObserverForReloadData(){
         let notifiReload = Notification.Name(notificationForLoadData)
         NotificationCenter.default.addObserver(self, selector: #selector(MovieDetailesTableViewController.reloadData) , name: notifiReload, object: nil)
     }
+    /**
+     This function called when notification is pushed
+     */
     @objc func reloadData(notification:NSNotification){
         DispatchQueue.main.async {
             self.lblRatting.text = self.viewModel?.getRating()
